ecd1478d0d60b71797c0abb01a74fbd96ca00f86
Kamal Bisht <kamal.bisht@daffodilsw.com>
2021-12-02 18:49:25 +0530

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { CdkDragDropComponent } from './cdk-drag-drop/cdk-drag-drop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    CdkDragDropComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cdk-drag-drop/cdk-drag-drop.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CdkDragDropRoutingModule } from './cdk-drag-drop-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CdkDragDropRoutingModule
+  ]
+})
+export class CdkDragDropModule { }
